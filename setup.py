83cc67ebae557f17dd0703f3a574a48e94f42992
Alireza <Alirezasampoor@gmail.com>
2026-08-15 20:46:31 +0330

    build: declare modern Python and dependency support

--- setup.py
+++ setup.py
@@ -1,32 +1,36 @@
 #!/usr/bin/python
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
-install_requires = \
-    ['requests>=2.18.4',
-     'websockets>=4.0.1'
-     ]
+install_requires = [
+    'requests>=2.31,<3',
+    'websockets>=14.1,<16',
+]
 
 setup(
     name='signalr-client-aio',
-    version='0.0.1.6.2',
+    version='0.0.2',
     author='Stanislav Lazarov',
     author_email='s.a.lazarov@gmail.com',
     license='MIT',
     url='https://github.com/slazarov/python-signalr-client',
     packages=find_packages(exclude=['tests*']),
     install_requires=install_requires,
-    description='Simple python SignalR client using asyncio.',
+    python_requires='>=3.9',
+    description='Simple Python SignalR client using asyncio.',
     download_url='https://github.com/slazarov/python-signalr-client.git',
-    keywords=['signalr', 'sginalr-weboscket', 'signalr-client', 'signalr-asyncio', 'signalr-aio'],
+    keywords=['signalr', 'signalr-websocket', 'signalr-client', 'signalr-asyncio', 'signalr-aio'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Intended Audience :: Information Technology',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
-    ]
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+    ],
 )
